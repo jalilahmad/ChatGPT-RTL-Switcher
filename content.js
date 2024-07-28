50b0cabe4765778fe6accd3492a47934d7c0a9f3
جلیل احمد <jalilahmad.fazeli@hotmail.com>
2024-07-28 14:52:58 +0430

    Add files via upload

--- content.js
+++ content.js
@@ -1,33 +1,32 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === "applyDirection") {
-    applyDirection(request.rtlState);
+    chrome.storage.local.get(['rtlState'], (result) => {
+      const isRTL = result.rtlState;
+      applyDirection(isRTL);
+    });
+  }
+
+  function applyDirection(isRTL) {
+    const dir = isRTL ? 'rtl' : 'auto';
+    const elements = document.querySelectorAll('.w-full.text-token-text-primary [dir]');
+    elements.forEach((element) => {
+      if (!element.classList.contains('overflow-y-auto')) {
+        element.setAttribute('dir', dir);
+      }
+    });
   }
 });
 
-function applyDirection(isRTL) {
-  const dir = isRTL ? 'rtl' : 'auto';
-  const elements = document.querySelectorAll('.w-full.text-token-text-primary [dir]');
-  
-  elements.forEach((element) => {
-    if (!element.classList.contains('overflow-y-auto')) {
-      element.setAttribute('dir', dir);
-    }
-  });
-}
-
-// نظارت بر تغییرات DOM
-new MutationObserver(mutations => {
-  mutations.forEach(mutation => {
-    mutation.addedNodes.forEach(node => {
-      if (node.nodeType === 1) { // Ensure the node is an element
-        if (node.matches('.w-full.text-token-text-primary [dir]') && !node.classList.contains('overflow-y-auto')) {
-          chrome.storage.local.get(['rtlState'], (result) => {
-            const isRTL = result.rtlState;
-            const dir = isRTL ? 'rtl' : 'auto';
-            node.setAttribute('dir', dir);
-          });
-        }
+// چک کردن و به‌روزرسانی جهت‌دهی (dir) برای عناصر جدید به طور منظم
+setInterval(() => {
+  chrome.storage.local.get(['rtlState'], (result) => {
+    const isRTL = result.rtlState;
+    const dir = isRTL ? 'rtl' : 'auto';
+    const elements = document.querySelectorAll('.w-full.text-token-text-primary [dir]');
+    elements.forEach((element) => {
+      if (!element.classList.contains('overflow-y-auto')) {
+        element.setAttribute('dir', dir);
       }
     });
   });
-}).observe(document.body, { childList: true, subtree: true });
+}, 1000); // هر 1 ثانیه چک کردن
